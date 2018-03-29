7cd4fd45ae581b4fcf93c6248807d7cbf63dc5ac
SamuelHallam <samuel.hallam@icloud.com>
2018-03-29 23:11:00 +0100

    changed a lot; added methods and now using requests

--- run.py
+++ run.py
@@ -4,33 +4,29 @@ import argparse
 from urllib.parse import quote_plus
 import platform
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
 
-__version__ = "0.2.1"
+__version__ = "0.3.1"
 app = Flask(__name__)
 
 parser = argparse.ArgumentParser(description='Search for movies')
 parser.version = "Movie Search {} on {}".format(__version__, platform.platform())
 parser.add_argument('--key', type=str, nargs=1, required=True)
-parser.add_argument('query', metavar='query string', type=str, nargs='+',
-                    help='The input to search for')
 parser.add_argument('--version', action='version')
 
 args = parser.parse_args()
-
 api_key = args.key
-search_query = quote_plus(" ".join(args.query))
-
-req = requests.get("http://omdbapi.com/?t={}&apikey={}".format(search_query, api_key[0]))
-movie = req.json()
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def home():
-    if movie['Response'] == "False":
-        return render_template("home.html", movie=movie['Error'])
-    else:
+    if request.method == 'POST':
+        search_query = quote_plus(request.form['movie'])
+        req = requests.get("http://omdbapi.com/?t={}&apikey={}".format(search_query, api_key[0]))
+        movie = req.json()
         return render_template("home.html", movie=movie)
+    else:
+        return render_template("home.html")
 
 
 if __name__ == "__main__":
